c7ade8c36099dd8c72b1577ee0c99aa120be749e
Silvio Watakabe <swat2014@outlook.com>
2025-02-06 09:59:35 -0500

    Implementação da camada de dominio

---
+++ src/main/java/me/dio/domain/model/BaseItem.java
@@ -0,0 +1,51 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.*;
+
+@MappedSuperclass
+public abstract class BaseItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String icon;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
